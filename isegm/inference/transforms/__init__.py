801e03ee9f42871f7682313097aa0db6b8234e82
Jonas <jonas_elmesten@hotmail.com>
2022-01-07 15:06:31 +0100

    Removed unused transformer.

--- isegm/inference/transforms/__init__.py
+++ isegm/inference/transforms/__init__.py
@@ -1,6 +1,5 @@
 from .base import SigmoidForPred
 from .flip import AddHorizontalFlip
 from .zoom_in import ZoomIn
-from .zoom_out import ZoomOut
 from .limit_longest_side import LimitLongestSide
 from .crops import Crops
